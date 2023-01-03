491574decf1a2384ecf424a1a125ff8c1bcfd6ea
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2023-01-03 10:06:46 -0400

    [update] adicionar critica vinculado ao banco

--- src/js/adicionarCritica.js
+++ src/js/adicionarCritica.js
@@ -1,3 +1,82 @@
 import { exibirCabecalho } from "../js/base.js";
+import { buscarCritica, criarCritica, editarCritica } from "../js/repositorios/criticasRepositorio.js";
+import { buscarLivro, buscarReferenciaLivro } from "../js/repositorios/livrosRepositorio.js";
 
-exibirCabecalho();
+inicializar();
+
+
+async function inicializar() {
+    exibirCabecalho();
+    let idLivro = obterIdLivro();
+    let idCritica = obterIdCritica();
+
+    if (idCritica != null) { //editar
+        // mostrar informações da critica
+        document.getElementById("titulo").textContent = "EDITAR CRITICA";
+        let criticaBanco = await buscarCritica(idCritica);
+        document.getElementById('userName').value = criticaBanco.username;
+        document.getElementById('critica').value = criticaBanco.critica;
+    }
+
+    var livro = await buscarLivro(idLivro);
+    document.getElementById('livro-atual').textContent = livro.titulo;
+
+    const btnEnviar = document.getElementById("cadastro-critica");
+    btnEnviar.addEventListener('click', async function () {
+        let dadosCritica;
+        let critica = document.getElementById('critica').value;
+
+        if (validacao(critica)) {
+            let date = getDate();
+            let codigoLivro = await buscarReferenciaLivro(idLivro);
+
+            dadosCritica = {
+                "critica": critica,
+                "data": date,
+                "codigoLivro": codigoLivro
+            }
+
+            if (idCritica != null) {
+                await editarCritica(idCritica, dadosCritica);
+            }
+            else {
+                await criarCritica(dadosCritica);
+            }
+
+            setTimeout(window.location.href = "detalhesLivro.html?idLivro=" + idLivro, 500);
+        }
+    });
+}
+
+function obterIdLivro() {
+    // Get idLivro in url
+    // example url: http://127.0.0.1:5500/templates/detalhesLivro.html?idLivro=zAe2KrUkM7T3XlZJNW3F
+    const urlParams = new URLSearchParams(window.location.search);
+    const idLivro = urlParams.get('idLivro')
+    return idLivro;
+}
+
+function obterIdCritica() {
+    // Get idLivro in url
+    // example url: http://127.0.0.1:5500/templates/adicionarCritica.html?idLivro=zAe2KrUkM7T3XlZJNW3F&&idCritica=XbapO3v3BElWfIIsA4Fh
+    const urlParams = new URLSearchParams(window.location.search);
+    const idCritica = urlParams.get('idCritica')
+    return idCritica;
+}
+
+function validacao(userName, critica) {
+    if (critica == "") {
+        window.alert("Preencha a descrição da critica");
+        return false;
+    }
+
+    return true;
+}
+
+function getDate() {
+    let data = new Date();
+    let dia = String(data.getDate()).padStart(2, '0');
+    let mes = String(data.getMonth() + 1).padStart(2, '0');
+    let ano = data.getFullYear();
+    return dia + '/' + mes + '/' + ano;
+}
--- src/js/detalhesLivro.js
+++ src/js/detalhesLivro.js
@@ -1,4 +1,4 @@
-import { adicionarCritica, exibirCabecalho } from "../js/base.js";
+import { adicionarCritica, exibirCabecalho, redirecionarPara } from "../js/base.js";
 import { buscarLivro } from "../js/repositorios/livrosRepositorio.js";
 import { buscarCriticasLivro, deletarCritica } from "../js/repositorios/criticasRepositorio.js";
 
@@ -11,9 +11,9 @@ function inicializar() {
 
     const btnCadastroCritica = document.getElementById("adicionarCritica");
     btnCadastroCritica.addEventListener('click', () => {
-        adicionarCritica();
+        let caminho = window.location.href = "adicionarCritica.html?idLivro=" + idLivro;
+        redirecionarPara(caminho);
     })
-
 }
 
 function obterIdLivro() {
@@ -46,13 +46,6 @@ async function preencherDadosCritica(livro) {
         criticas.forEach((critica) => {
             criarCriticaHTML(critica);
         });
-
-        document.querySelectorAll('.delete').forEach(botao => {
-            botao.addEventListener('click', () => { deletar(botao.id) })
-        })
-        document.querySelectorAll('.edit').forEach(botao => {
-            botao.addEventListener('click', () => { editar(botao.id) })
-        })
     }
 }
 
@@ -61,22 +54,8 @@ function criarCriticaHTML(critica) {
     let container = document.getElementById('criticas-livro');
     let criticaDiv = `
         <div class='critica'> 
-            <p class='critica-username' id='username'>${critica.username}</p>
             <p class='critica-data' id='data'>${critica.data}</p>
             <p class='critica-descricao texto' id='criticaDescricao'>${critica.critica}</p>
-            <div class='acoes'>
-                <button class='btn-icon delete' id='deletar${critica.id}'> 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                    </svg>
-                </button>
-                <button class='btn-icon edit' id='editar${critica.id}'> 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-                    </svg>
-                </button>
-            </div>
         </div>`
     container.innerHTML += criticaDiv;
 }
