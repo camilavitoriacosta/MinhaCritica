b0638f9b92335702c313bb4895266f8d2185a3be
camilacosta <camilavitoria.c.costa@gmail.com>
2023-01-11 16:26:40 -0400

    Tela de cadastro de usuarios funcionando

--- src/js/repositorios/usuarioRepositorio.js
+++ src/js/repositorios/usuarioRepositorio.js
@@ -1,24 +1,23 @@
 import { db, auth } from '../firebaseConfig.js';
 import { createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js';
 
-import { doc, setDoc, collection } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js';
+import { doc, setDoc, collection } from 'https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js';
 
 
 // ToDO: Mensagem para email já existe
 export function criarAutentificacao(autentificacao) {
-    // createUserWithEmailAndPassword(auth, autentificacao.email, autentificacao.senha)
-    //     .then((userCredential) => {
-    // const user = userCredential.user;
-    const uid = "a9xrgITQBEPUl2IaiJnGuOqhXvX2";
-    criarUsuario(uid, autentificacao);
-    // })
-    // .catch((error) => {
-    //   console.log(error);
-    // })
+    createUserWithEmailAndPassword(auth, autentificacao.email, autentificacao.senha)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            criarUsuario(user.uid, autentificacao);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
 }
 
 async function criarUsuario(chave, usuario) {
-    const usuarios = doc(collection(db, "usuarios"), chave);
+    let usuarios = doc(db, "usuarios", chave);
     await setDoc(usuarios, {
         username: usuario.username,
         email: usuario.email,
