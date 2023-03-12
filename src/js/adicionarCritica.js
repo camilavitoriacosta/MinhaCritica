d1d234400286bb88cfc8f43a0dc88cf437892e39
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2023-03-12 12:45:30 -0400

    Acesso a adicionar critica apenas para usuarios

--- src/js/adicionarCritica.js
+++ src/js/adicionarCritica.js
@@ -1,50 +1,56 @@
-import { exibirCabecalho } from "../js/base.js";
+import { exibirCabecalho, redirecionarPara } from "../js/base.js";
 import { criarCritica, editarCritica, buscarCritica } from "../js/repositorios/criticasRepositorio.js";
 import { buscarLivro, buscarReferenciaLivro } from "../js/repositorios/livrosRepositorio.js";
-import { obterUsuarioLogado } from "./usuarioAutentificacao.js";
+import { obterUsuarioLogado, usuarioLogado } from "./usuarioAutentificacao.js";
 import { validarCampos } from "./validadores/validacao.js";
 
 inicializar();
 
 
 async function inicializar() {
-    exibirCabecalho();
-    let idLivro = obterIdLivro();
-    let idCritica = obterIdCritica();
+    if (usuarioLogado()) {
+        exibirCabecalho();
+        let idLivro = obterIdLivro();
+        let idCritica = obterIdCritica();
 
-    let livro = await buscarLivro(idLivro);
-    document.getElementById('livro-atual').textContent = livro.titulo;
+        let livro = await buscarLivro(idLivro);
+        document.getElementById('livro-atual').textContent = livro.titulo;
 
-    if (idCritica != null) {
-        let critica = await buscarCritica(idCritica);
-        document.getElementById('critica').textContent = critica.critica;
+        if (idCritica != null) {
+            let critica = await buscarCritica(idCritica);
+            document.getElementById('critica').textContent = critica.critica;
+        }
+
+        const btnEnviar = document.getElementById("cadastro-critica");
+        btnEnviar.addEventListener('click', async function () {
+            let dadosCritica;
+            let critica = document.getElementById('critica');
+
+            if (validarCampos([critica])) {
+                let date = obterData();
+                let codigoLivro = await buscarReferenciaLivro(idLivro);
+
+                dadosCritica = {
+                    "critica": critica.value.trim(),
+                    "data": date,
+                    "codigoLivro": codigoLivro,
+                    "usuario": (await obterUsuarioLogado()).referencia
+                }
+
+                if (idCritica != null) {
+                    await editarCritica(idCritica, dadosCritica);
+                }
+                else {
+                    await criarCritica(dadosCritica);
+                }
+                setTimeout(window.location.href = "detalhesLivro.html?idLivro=" + idLivro, 500);
+            }
+        });
+    }
+    else{
+        redirecionarPara('../../index.html');
     }
 
-    const btnEnviar = document.getElementById("cadastro-critica");
-    btnEnviar.addEventListener('click', async function () {
-        let dadosCritica;
-        let critica = document.getElementById('critica');
-
-        if (validarCampos([critica])) {
-            let date = obterData();
-            let codigoLivro = await buscarReferenciaLivro(idLivro);
-
-            dadosCritica = {
-                "critica": critica.value.trim(),
-                "data": date,
-                "codigoLivro": codigoLivro,
-                "usuario": (await obterUsuarioLogado()).referencia
-            }
-
-            if (idCritica != null) {
-                await editarCritica(idCritica, dadosCritica);
-            }
-            else {
-                await criarCritica(dadosCritica);
-            }
-            setTimeout(window.location.href = "detalhesLivro.html?idLivro=" + idLivro, 500);
-        }
-    });
 }
 
 function obterIdLivro() {
